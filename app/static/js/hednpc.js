9d84655e717bcbb48d6f745a63096426d0b3504c
Zyntab <viktor.almen@gmail.com>
2018-01-17 13:45:55 +0100

    Add confirmation before deleting characters

--- app/static/js/hednpc.js
+++ app/static/js/hednpc.js
@@ -1,9 +1,11 @@
 function cancel_delete(charname) {
-	var url = "{{ url_for('character', charname=" + charname + ") }}";
+	var url = "{{ url_for('character', 'charname'=" + charname + ") }}";
+	alert(url);
 	window.location = url;
 }
 
 function confirm_delete(delname) {
-	var url = "{{ url_for('del_char', delname=" + charname + ") }}";
+	var url = "{{ url_for('del_char', delname=" + delname + ") }}";
+	alert(url);
 	window.location = url;
 }
