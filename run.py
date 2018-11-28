40fdc7772f893750d8b7b787ee974f413a4b3015
Zyntab <viktor.almen@gmail.com>
2018-11-28 19:51:49 +0100

    change run.py and runp.py, remove app.run()

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #! usr/bin/python
 from app import app, db
 from app.models import User, Character
-app.run(debug=True)
+#app.run(debug=True)
 
 @app.shell_context_processor
 def make_shell_context():
--- runp.py
+++ runp.py
@@ -1,3 +1,3 @@
 #! /usr/bin/python
 from app import app
-app.run(debug=False)
+#app.run(debug=False)
